85af61000270550eea8216d2642e53c53388930d
KasagiSoma <oibc36049@ojs.ac.jp>
2025-04-22 09:52:03 +0900

    軽微な修正

--- Chapter02/DistanceConverter/FeedConverter.cs
+++ Chapter02/DistanceConverter/FeedConverter.cs
@@ -8,7 +8,6 @@ namespace DistanceConverter {
     public static class FeedConverter {
         private const double ratio = 0.3048;
 
-
         /// <summary>メートル値をフィード値に変換し、一覧を出力します。</summary>
         /// <param name="_min">変換最小値</param>
         /// <param name="_max">変換最大値</param>
@@ -34,20 +33,23 @@ namespace DistanceConverter {
         /// <summary>フィード値をメートル値に変換します。</summary>
         /// <param name="_feet">変換元フィード値</param>
         /// <returns>メートル値</returns>
-        static double FeetToMeter(int _feet) {
+        private static double FeetToMeter(int _feet) {
             return _feet * ratio;
         }
 
         /// <summary>メートル値をフィード値に変換します。</summary>
         /// <param name="_meter">変換元メートル値</param>
         /// <returns>フィード値</returns>
-        static double MeterToFeet(int _meter) {
+        private static double MeterToFeet(int _meter) {
             return _meter / ratio;
         }
 
-        static string fillSpace(int _cnt) {
+        /// <summary>任意文字数の空白文字列を作成します。</summary>
+        /// <param name="_length">文字数</param>
+        /// <returns>引数で指定した文字数のスペース文字列</returns>
+        private static string fillSpace(int _length) {
             string rtn = "";
-            for (int i = 0; i < _cnt; i++) {
+            for (int i = 0; i < _length; i++) {
                 rtn += " ";
             }
             return rtn;
