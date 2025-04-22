f7b58037ad36d8bddf655e8a12a85fb4b8417624
KasagiSoma <oibc36049@ojs.ac.jp>
2025-04-22 11:41:01 +0900

    インチからメートル変換完成

---
+++ Chapter02/演習問題/Exercise02/InchConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    public static class InchConverter {
+        private const double oneInchToMeter = 0.0254;
+
+        /// <summary>インチをメートルに変換します。</summary>
+        /// <param name="_inch">変換元インチ</param>
+        /// <returns>変換後メートル</returns>
+        public static double InchToMeter(int _inch) {
+            double rtn = oneInchToMeter * _inch;
+            return rtn;
+        }
+    }
+}
---
+++ Chapter02/演習問題/Exercise02/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            PrintInchToMeter(1, 10);
+        }
+
+        /// <summary>
+        /// インチからメートルの変換表を出力します。
+        /// </summary>
+        /// <param name="_min">変換最小値</param>
+        /// <param name="_max">変換最大値</param>
+        private static void PrintInchToMeter(int _min, int _max) {
+            for (int i = _min; i <= _max; i++) {
+                double m = InchConverter.InchToMeter(i);
+                int sp = _max.ToString().Length - i.ToString().Length;
+                //FillSpace(sp)は、左詰めしようと{i,sp}としたら、「spの部分は定数にしろ」
+                //...と怒られたため、無理やりスペース埋めで
+                Console.WriteLine($"{FillSpace(sp)}{i}in = {m:0.0000}m");
+            }
+        }
+
+
+        /// <summary>任意文字数の空白文字列を作成します。</summary>
+        /// <param name="_length">文字数</param>
+        /// <returns>引数で指定した文字数のスペース文字列</returns>
+        private static string FillSpace(int _length) {
+            string rtn = "";
+            for (int i = 0; i < _length; i++) {
+                rtn += " ";
+            }
+            return rtn;
+        }
+    }
+}
