8366b55d23993032c053ac5083f3655cac08e2ef
KasagiSoma <oibc36049@ojs.ac.jp>
2025-04-22 10:31:28 +0900

    静的メソッドとして利用

--- Chapter02/DistanceConverter/FeetConverter.cs
+++ Chapter02/DistanceConverter/FeetConverter.cs
@@ -5,20 +5,20 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DistanceConverter {
-    public  class FeetConverter {
+    public static class FeetConverter {
         private const double ratio = 0.3048;
 
         /// <summary>フィート値をメートル値に変換します。</summary>
         /// <param name="_feet">変換元フィート値</param>
         /// <returns>メートル値</returns>
-        public  double FeetToMeter(int _feet) {
+        public static double FeetToMeter(int _feet) {
             return _feet * ratio;
         }
 
         /// <summary>メートル値をフィート値に変換します。</summary>
         /// <param name="_meter">変換元メートル値</param>
         /// <returns>フィート値</returns>
-        public  double MeterToFeet(int _meter) {
+        public static double MeterToFeet(int _meter) {
             return _meter / ratio;
         }
 
--- Chapter02/DistanceConverter/Program.cs
+++ Chapter02/DistanceConverter/Program.cs
@@ -25,46 +25,52 @@
             }
         }
 
+
+        //以下、範囲変換メソッド
+
         /// <summary>メートル値をフィート値に変換し、一覧を出力します。</summary>
         /// <param name="_min">変換最小値</param>
         /// <param name="_max">変換最大値</param>
         private static void MeterToFeet(int _min, int _max) {
-            FeetConverter fc = new FeetConverter();
             for (int meter = _min; meter <= _max; meter++) {
                 int sp = _max.ToString().Length - meter.ToString().Length;
-                double feet = fc.MeterToFeet(meter);
+                double feet = FeetConverter.MeterToFeet(meter);
                 Console.WriteLine($"{fillSpace(sp)}{meter}m = {feet:0.0000}fr");
             }
         }
-        /// <summary>メートル値をフィート値に変換し、出力します。</summary>
-        /// <param name="_val">変換元値</param>
-        private static void MeterToFeet(int _val) {
-            FeetConverter fc = new FeetConverter();
-            double feet = fc.MeterToFeet(_val);
-            Console.WriteLine($"{_val}m = {feet:0.0000}fr");
-
-        }
-
+      
         /// <summary>フィート値をメートル値に変換し、一覧を出力します。</summary>
         /// <param name="_min">変換最小値</param>
         /// <param name="_max">変換最大値</param>
         private static void FeetToMeter(int _min, int _max) {
-            FeetConverter fc = new FeetConverter();
             for (int feet = _min; feet <= _max; feet++) {
                 int sp = _max.ToString().Length - feet.ToString().Length;
-                double meter = fc.FeetToMeter(feet);
+                double meter = FeetConverter.FeetToMeter(feet);
                 Console.WriteLine($"{fillSpace(sp)}{feet}fr = {meter:0.0000}m");
             }
         }
+
+
+        //以下、単一変換メソッド
+
+        /// <summary>メートル値をフィート値に変換し、出力します。</summary>
+        /// <param name="_val">変換元値</param>
+        private static void MeterToFeet(int _val) {
+            double feet = FeetConverter.MeterToFeet(_val);
+            Console.WriteLine($"{_val}m = {feet:0.0000}fr");
+
+        }
+
         /// <summary>フィート値をメートル値に変換し、出力します。</summary>
         /// <param name="_val">変換元値</param>
         private static void FeetToMeter(int _val) {
-            FeetConverter fc = new FeetConverter();
-            double meter = fc.FeetToMeter(_val);
+            double meter = FeetConverter.FeetToMeter(_val);
             Console.WriteLine($"{_val}fr = {meter:0.0000}m");
 
         }
 
+
+        //以下、授業でやってないメソッド
 
         /// <summary>任意文字数の空白文字列を作成します。</summary>
         /// <param name="_length">文字数</param>
@@ -82,8 +88,8 @@
         /// <param name="_failed">変換失敗時に返り値となる数値</param>
         /// <returns>変換した数値 または 指定された数値(失敗時：_failed)</returns>
         private static int TryParceInt(string _baseStr, int _failed) {
-            Console.WriteLine("不明な値：" + _baseStr + "は無視されました。");
             if (int.TryParse(_baseStr, out int val)) return val;
+            Console.WriteLine("不明な値：" + _baseStr + "は無視されました。");
             return _failed;
         }
 
