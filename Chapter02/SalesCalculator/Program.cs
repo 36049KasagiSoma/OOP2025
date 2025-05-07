8cbc6b6e0e01e8924e320ca843e466fd6f417d8b
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-07 11:26:41 +0900

    Sale読み込み完成

---
+++ Chapter02/SalesCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SalesCalculator {
+    internal class Program {
+        static void Main(string[] args) {
+            List<Sale> s = ReadSales(@"data\Sales.csv");
+            Console.WriteLine(s.Count);
+        }
+
+        static List<Sale> ReadSales(string filePath) {
+            //売り上げリスト
+            List<Sale> sales = new List<Sale>();
+            string[] lines = File.ReadAllLines(filePath);
+            //各行をカンマ区切りで分割、Saleデータの作成、登録
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Sale sale = new Sale {
+                    ShopName = items[0],
+                    ProductCategory = items[1],
+                    Amount = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+
+            return sales;
+        }
+    }
+}
---
+++ Chapter02/SalesCalculator/Sale.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    /// <summary>売上クラス</summary>
+    class Sale {
+        /// <summary>店舗名</summary>
+        public string ShopName { get; set; } = String.Empty;
+        /// <summary>商品カテゴリー</summary>
+        public string ProductCategory { get; set; } = String.Empty;
+        /// <summary>売上高</summary>
+        public int Amount { get; set; }
+    }
+}
