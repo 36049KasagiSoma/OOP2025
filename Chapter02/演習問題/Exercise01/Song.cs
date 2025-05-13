9cc6196bcd991331c02fb3268b151ab546266c60
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-13 09:34:42 +0900

    問題2.1.1 完成

---
+++ Chapter02/演習問題/Exercise01/Song.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class Song {
+        public string Title { get; set; } = String.Empty;
+        public string ArtistName { get; set; } = String.Empty;
+        public int Length { get; set; }
+    }
+}
