f07dd731266e47f455c70ca998a7c1e8ae5e7e7f
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-13 10:11:27 +0900

    問題2.1.4 完成

--- Chapter02/演習問題/Exercise01/Program.cs
+++ Chapter02/演習問題/Exercise01/Program.cs
@@ -9,6 +9,16 @@
                 new Song("Honesty", "Billy Joel", 231),
                 new Song("I Will Always Love You", "Whitney Houston", 273),
             };
+
+            printSongs(songs);
+        }
+
+        //2.1.4
+        private static void printSongs(Song[] songs) {
+            foreach (var song in songs) {
+                var dt = new TimeSpan(0, 0, song.Length);
+                Console.WriteLine($"title:{song.Title,-30}artist:{song.ArtistName,-20}length:{dt.ToString(@"mm\:ss")}");
+            }
         }
 
     }
