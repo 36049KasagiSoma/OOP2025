2fbabce70ac68469966a25a45e68e13f695f7847
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-14 11:42:18 +0900

    コード3.1完成

---
+++ Chapter03/Section01/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.Write("カウントしたい数値：");
+            if (int.TryParse(Console.ReadLine(), out int num)) {
+                Console.WriteLine(Count(num));
+            } else {
+                Console.WriteLine("無効な数値");
+            }
+        }
+
+        static int Count(int num) {
+            var numvers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var count = 0;
+            foreach (var n in numvers) {
+                if (n == num) {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
