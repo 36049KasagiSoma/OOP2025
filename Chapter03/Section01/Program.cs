409b75ad3429e3c1e7d461374798b81e0b74bdd4
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-16 14:02:28 +0900

    P65まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -1,25 +1,15 @@
 ﻿namespace Section01 {
     internal class Program {
-
-        public delegate bool Judgement(int value); //デリゲートの宣言
-
+        
         static void Main(string[] args) {
 
             var numbers = new int[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            Judgement judge = IsOdd;
-            Console.WriteLine(Count(numbers, judge));
+            Console.WriteLine(Count(numbers, delegate(int n) { return n % 2 == 0; }));
 
         }
 
-        static bool IsEven(int n) {
-            return n % 2 == 0;
-        }
 
-        static bool IsOdd(int n) {
-            return n % 2 != 0;
-        }
-
-        static int Count(int[] numvers, Judgement judge) {
+        static int Count(int[] numvers, Func<int,bool> judge) {
             var count = 0;
             foreach (var n in numvers) {
                 //引数で受け取ったメソッドを呼び出す
@@ -29,5 +19,27 @@
             }
             return count;
         }
+
+        /*
+         * Actionデリゲート
+         * 戻り値なし
+         * 
+         * Action<T>
+         * 一つの引数（型はT）
+         * 
+         * 
+         * Predicateデリゲート
+         * 戻り値はbool
+         * 
+         * Predicate<T>
+         * 一つの引数（型はT）
+         * 
+         * 
+         * Funcデリゲート
+         * 任意の戻り値（型はTResult）
+         * 
+         * Func<T,TResult>
+         * 一つの引数（型はT）
+         */
     }
 }
