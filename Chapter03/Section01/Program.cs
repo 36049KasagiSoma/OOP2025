faae5352e82ac7367bf28cb0d1b35421e2373260
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-16 14:56:54 +0900

    p71まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -2,44 +2,28 @@
     internal class Program {
 
         static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            var numbers = new int[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            Console.WriteLine(Count(numbers, n => (n >= 5 && n < 10)));
+            var lowerList = cities.ConvertAll(s => s.ToLower());
+            lowerList.ForEach(Console.WriteLine);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var upperList = cities.ConvertAll(s => s.ToUpper());
+            upperList.ForEach(Console.WriteLine);
 
         }
 
 
-        static int Count(int[] numvers, Func<int, bool> judge) {
-            var count = 0;
-            foreach (var n in numvers) {
-                //引数で受け取ったメソッドを呼び出す
-                if (judge(n)) {
-                    count++;
-                }
-            }
-            return count;
-        }
-
-        /*
-         * Actionデリゲート
-         * 戻り値なし
-         * 
-         * Action<T>
-         * 一つの引数（型はT）
-         * 
-         * 
-         * Predicateデリゲート
-         * 戻り値はbool
-         * 
-         * Predicate<T>
-         * 一つの引数（型はT）
-         * 
-         * 
-         * Funcデリゲート
-         * 任意の戻り値（型はTResult）
-         * 
-         * Func<T,TResult>
-         * 一つの引数（型はT）
-         */
     }
 }
