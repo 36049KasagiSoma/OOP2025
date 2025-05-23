ead7293517b90bf3dfdf95caaf5d6bd24cbbd989
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-23 15:25:42 +0900

    4.2.1～4.2.3完成

--- Chapter04/演習問題/Exercise01/Program.cs
+++ Chapter04/演習問題/Exercise01/Program.cs
@@ -19,23 +19,32 @@ namespace Exercise01 {
             foreach (var s in langs) {
                 if (s.Contains('S')) Console.WriteLine(s);
             }
+
+            Console.WriteLine();
             Console.WriteLine("= [for] ==============");
             for (int i = 0; i < langs.Count; i++) {
                 if (langs[i].Contains('S')) Console.WriteLine(langs[i]);
             }
+
+            Console.WriteLine();
             Console.WriteLine("= [while] ============");
             int index = 0;
             while (index < langs.Count) {
                 if (langs[index].Contains('S')) Console.WriteLine(langs[index]);
                 index++;
             }
+            Console.WriteLine();
         }
 
         private static void Exercise2(List<string> langs) {
             langs.ForEach(s => {
                 if (s.Contains('S')) Console.WriteLine(s);
             });
-            //langs.Where(s => s.Contains('S')).ToList().ForEach(Console.WriteLine);
+
+            Console.WriteLine();
+
+            //var filtedLangs = langs.Where(s => s.Contains('S'));
+            //foreach (var lang in filtedLangs) Console.WriteLine(lang);
         }
 
         private static void Exercise3(List<string> langs) {
---
+++ Exercise02/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1();
+            Console.WriteLine("---");
+            Exercise2();
+            Console.WriteLine("---");
+            Exercise3();
+        }
+
+        private static void Exercise1() {
+            int value = GetinputInt("数値を入力:");
+
+            if (value < 0) {
+                Console.WriteLine(value);
+            } else if (value < 100) {
+                Console.WriteLine(value * 2);
+            } else if (value < 500) {
+                Console.WriteLine(value * 3);
+            } else {
+                Console.WriteLine(value);
+            }
+
+        }
+
+        private static void Exercise2() {
+            int value = GetinputInt("数値を入力:");
+
+            switch (value) {
+                case < 0:
+                    Console.WriteLine(value);
+                    break;
+                case < 100:
+                    Console.WriteLine(value * 2);
+                    break;
+                case < 500:
+                    Console.WriteLine(value * 3);
+                    break;
+                default:
+                    Console.WriteLine(value);
+                    break;
+            }
+        }
+
+        private static void Exercise3() {
+            int value = GetinputInt("数値を入力:");
+
+            Console.WriteLine(value switch {
+                < 0 => value,
+                < 100 => value * 2,
+                < 500 => value * 3,
+                _ => value
+            });
+        }
+
+        private static int GetinputInt(string tips) {
+            int value;
+            string line;
+            do {
+                Console.Write(tips);
+                line = Console.ReadLine() ?? "";
+            } while (!int.TryParse(line, out value));
+
+            return value;
+        }
+    }
+}
