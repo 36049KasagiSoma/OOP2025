2978715228b127ad0fa39be92d5e2c5e7129f7fb
KasagiSoma <oibc36049@ojs.ac.jp>
2025-05-27 10:11:22 +0900

    7.1.5まで完成

--- LINQ/LinqSample/Program.cs
+++ LINQ/LinqSample/Program.cs
@@ -1,7 +1,19 @@
 ﻿namespace LinqSample {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var numbers = Enumerable.Range(1, 100);
+
+            //合計
+            Console.WriteLine("偶数合計:" + numbers.Where(n => n % 8 == 0).Sum());
+
+            //==================================
+
+            //Console.WriteLine("偶数合計:" + numbers.Sum(n => (n % 2 == 0 ? n : 0)));
+
+            //Console.WriteLine("2倍偶数合計:" + numbers.Sum(n => (n % 2 == 0 ? n * 2 : 0)));
+
+
+
         }
     }
 }
---
+++ chapter07/演習問題/Exercise01/Program.cs
@@ -0,0 +1,47 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int[] numbers = [5, 10, 17, 9, 3, 21, 10, 40, 21, 3, 35];
+
+            Console.WriteLine("--7.1.1----------");
+            Exercise1(numbers);
+
+            Console.WriteLine("--7.1.2----------");
+            Exercise2(numbers);
+
+            Console.WriteLine("--7.1.3----------");
+            Exercise3(numbers);
+
+            Console.WriteLine("--7.1.4----------");
+            Exercise4(numbers);
+
+            Console.WriteLine("--7.1.5----------");
+            Exercise5(numbers);
+        }
+
+        private static void Exercise1(int[] numbers) {
+            Console.WriteLine(numbers.Max());
+        }
+
+        private static void Exercise2(int[] numbers) {
+            numbers[^2..].ToList().ForEach(Console.WriteLine);
+
+            //[n..m] ... nからmまでを取り出す。
+            //  ^n   ... 最後からn番目
+            //参考:https://qiita.com/aramasa_ash/items/e3032f04cc0535529bb6
+        }
+
+        private static void Exercise3(int[] numbers) {
+            numbers.ToList().ForEach(n => Console.WriteLine(n.ToString("000")));
+        }
+
+        private static void Exercise4(int[] numbers) {
+            numbers.Order().Take(3).ToList().ForEach(Console.WriteLine);
+        }
+
+        private static void Exercise5(int[] numbers) {
+            Console.WriteLine(numbers.Distinct().Count(n => n > 10));
+        }
+    }
+}
