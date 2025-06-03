82b36e52d3da29a2dface5ec4c5a0ac89b4f2952
KasagiSoma <oibc36049@ojs.ac.jp>
2025-06-03 11:35:37 +0900

    6.3.3完成

---
+++ Chapter06/Section03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace Section03 {
+    internal class Program {
+        static void Main(string[] args) {
+            var sb = new StringBuilder();
+            foreach (var word in GetWorks()) {
+                sb.Append(word);
+            }
+
+            Console.WriteLine(sb.ToString());
+
+            string str = "";
+            foreach (var word in GetWorks()) {
+                str += word;
+            }
+
+            Console.WriteLine(str);
+        }
+
+        private static IEnumerable<string> GetWorks() {
+            return ["Orange", "Lemon", "Strawberry"];
+        }
+    }
+}
