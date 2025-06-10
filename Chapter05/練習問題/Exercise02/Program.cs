cba0e4e6781b9f8bcf1e46674a74826c400d7208
KasagiSoma <oibc36049@ojs.ac.jp>
2025-06-10 11:24:47 +0900

    5.2.4null合体演算子を無理やり使う

--- Chapter05/練習問題/Exercise02/Program.cs
+++ Chapter05/練習問題/Exercise02/Program.cs
@@ -7,10 +7,11 @@ namespace Exercise02 {
             var ymCollection = new YearMonth[] {
                 new YearMonth(1980, 1),
                 new YearMonth(1990, 4),
-                new YearMonth(2000, 7),
-                new YearMonth(2010, 9),
-                new YearMonth(2024, 12),
+                new YearMonth(1000, 7),
+                new YearMonth(1010, 9),
+                new YearMonth(1024, 12),
             };
+
 
             Console.WriteLine("=====5.2.2==========");
             Exercise2(ymCollection);
@@ -28,6 +29,11 @@ namespace Exercise02 {
             ymCollection.ToList().ForEach(Console.WriteLine);
         }
 
+        /// <summary>
+        /// 与えられたYearMonth配列から、一番最初の21世紀のデータを返します。
+        /// </summary>
+        /// <param name="ymCollection">検索配列</param>
+        /// <returns>最初の21世紀のデータ（見つからなかったらnull）</returns>
         private static YearMonth? FindFirst21C(YearMonth[] ymCollection) {
             foreach (YearMonth y in ymCollection) {
                 if (y.Is21Century) return y;
@@ -37,7 +43,20 @@ namespace Exercise02 {
 
         private static void Exercise4(YearMonth[] ymCollection) {
             YearMonth? y = FindFirst21C(ymCollection);
-            Console.WriteLine(y is not null ? y.Year : "21世紀のデータはありません");
+            //Console.WriteLine(y is not null ? y.Year : "21世紀のデータはありません");
+
+            //null合体演算子
+            //Console.WriteLine(SafetyGetYear(y) ?? "21世紀のデータはありません");
+        }
+
+        /// <summary>
+        /// YearMonthから安全に年を取得します。
+        /// </summary>
+        /// <param name="y">取得対象YearMonth</param>
+        /// <returns>年（引数がnullの場合はnull）</returns>
+        public static string? SafetyGetYear(YearMonth? y) {
+            if (y == null) return null;
+            return y.Year.ToString();
         }
 
         private static void Exercise5(YearMonth[] ymCollection) {
@@ -46,4 +65,5 @@ namespace Exercise02 {
             Exercise2(newYm);
         }
     }
+
 }
