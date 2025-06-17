77f34712a4dcafa4996e6ea98737e124e639c63b
KasagiSoma <oibc36049@ojs.ac.jp>
2025-06-17 11:07:49 +0900

    設問1_Srudentクラス完成

---
+++ Test01/Test01/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test01 {
+    public class Student {
+        public required string Name { get; init; }
+        public required string Subject { get; init; }
+        public required int Score { get; init; }
+    }
+}
