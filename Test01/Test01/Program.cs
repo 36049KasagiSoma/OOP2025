01ef5ad3470b98471743fd21ddbea6a94048ea61
KasagiSoma <oibc36049@ojs.ac.jp>
2025-06-17 11:50:54 +0900

    設問1_コメント入力 & インスタンス名がsaleだったのでstudentに修正

--- Test01/Test01/Program.cs
+++ Test01/Test01/Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Test01
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
+﻿namespace Test01 {
+    public class Program {
+        static void Main(string[] args) {
+            //情報の取り込み
             var score = new ScoreCounter("StudentScore.csv");
+
+            //教科別集計結果取得
             var TotalBySubject = score.GetPerStudentScore();
             foreach (var obj in TotalBySubject) {
                 Console.WriteLine("{0} {1}", obj.Key, obj.Value);
--- Test01/Test01/ScoreCounter.cs
+++ Test01/Test01/ScoreCounter.cs
@@ -1,37 +1,46 @@
 ﻿namespace Test01 {
+    /// <summary>
+    /// ファイルから情報を読み込み、集計をサポートするクラスです。
+    /// </summary>
     public class ScoreCounter {
         private IEnumerable<Student> _score;
 
-        // コンストラクタ
+        /// <param name="filePath">読み込み対象のファイルのパス</param>
         public ScoreCounter(string filePath) {
             _score = ReadScore(filePath);
         }
 
         //メソッドの概要：
-        //引数で指定されたパスのファイルから「,」区切りのデータを読み取り、Studentに変換してIEnumerableを返す。
+        //指定したファイルパスから、名前、教科、点数の一覧を取得する。
         private static IEnumerable<Student> ReadScore(string filePath) {
-            //点数リスト
             var scores = new List<Student>();
             var lines = File.ReadAllLines(filePath);
-            //各行をカンマ区切りで分割、Scoreデータの作成、登録
+
+            //分割、登録処理
             foreach (var line in lines) {
                 var items = line.Split(',');
+
+                //第三引数はintなのでint変換
                 int score = int.Parse(items[2]);
-                var sale = new Student {
+
+                var student = new Student {
                     Name = items[0],
                     Subject = items[1],
                     Score = score
                 };
-                scores.Add(sale);
+                scores.Add(student);
             }
             return scores;
         }
 
         //メソッドの概要：
-        //保持しているStudentListから教科ごとの合計点数を求め、教科名をキーとした点数の一覧を返す。
+        //教科ごとの合計点数を求め、教科名をキーとした点数の一覧を返す。
         public IDictionary<string, int> GetPerStudentScore() {
             var dict = new Dictionary<string, int>();
+            
+            //集計処理
             foreach (var scorePair in _score) {
+                //登録済み、未登録での処理分岐
                 if (dict.ContainsKey(scorePair.Subject)) {
                     dict[scorePair.Subject] += scorePair.Score;
                 } else {
