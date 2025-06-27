49747101c9658b21ee4fa874a260d32cae3271c1
KasagiSoma <oibc36049@ojs.ac.jp>
2025-06-27 13:50:06 +0900

    パート2終わり

--- Chapter09/演習問題/Exercise03/Program.cs
+++ Chapter09/演習問題/Exercise03/Program.cs
@@ -3,20 +3,22 @@
 namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
+            int sleapTime = 1500;
 #if false
             var tw = new TimeWatch();
             tw.Start();
             // スリープする
-            Thread.Sleep(1000);
+            Thread.Sleep(sleapTime);
             TimeSpan duration = tw.Stop();
             Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
 #else
             var tw = new Stopwatch();
             tw.Start();
             // スリープする
-            Thread.Sleep(1000);
+            Thread.Sleep(sleapTime);
             tw.Stop();
-            Console.WriteLine("処理時間は{0}ミリ秒でした", ((double)tw.ElapsedTicks/10000));
+            TimeSpan duration = tw.Elapsed;
+            Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
 #endif
         }
 
