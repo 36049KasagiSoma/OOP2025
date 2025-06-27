a276622d5df412836806a1b3d28201c30b50d91e
KasagiSoma <oibc36049@ojs.ac.jp>
2025-06-27 13:26:55 +0900

    Tick変更

--- Chapter09/演習問題/Exercise03/Program.cs
+++ Chapter09/演習問題/Exercise03/Program.cs
@@ -1,27 +1,37 @@
-﻿namespace Exercise03 {
+﻿using System.Diagnostics;
+
+namespace Exercise03 {
     internal class Program {
         static void Main(string[] args) {
+#if false
             var tw = new TimeWatch();
             tw.Start();
             // スリープする
             Thread.Sleep(1000);
             TimeSpan duration = tw.Stop();
             Console.WriteLine("処理時間は{0}ミリ秒でした", duration.TotalMilliseconds);
-
-        }
-    }
-
-    class TimeWatch {
-        private DateTime _time;
-        
-        public void Start() {
-            //現在の時間を_timeに設定
-            _time = DateTime.Now;
+#else
+            var tw = new Stopwatch();
+            tw.Start();
+            // スリープする
+            Thread.Sleep(1000);
+            tw.Stop();
+            Console.WriteLine("処理時間は{0}ミリ秒でした", ((double)tw.ElapsedTicks/10000));
+#endif
         }
 
-        public TimeSpan Stop() {
-            //経過時間を返却する
-            return DateTime.Now - _time;
+        class TimeWatch {
+            private DateTime _time;
+
+            public void Start() {
+                //現在の時間を_timeに設定
+                _time = DateTime.Now;
+            }
+
+            public TimeSpan Stop() {
+                //経過時間を返却する
+                return DateTime.Now - _time;
+            }
         }
     }
 }
