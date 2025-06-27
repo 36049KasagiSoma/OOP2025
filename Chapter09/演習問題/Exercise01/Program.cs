5f68a4b3535b1a754a0feb52b7c34f0b56d001f5
KasagiSoma <oibc36049@ojs.ac.jp>
2025-06-27 10:12:18 +0900

    9.2.3まで完成

---
+++ Chapter09/演習問題/Exercise01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+
+            DisplayPattern1(dateTime);
+            DisplayPattern2(dateTime);
+            DisplayPattern3(dateTime);
+        }
+
+        private static void DisplayPattern1(DateTime dateTime) {
+            Console.WriteLine(string.Format("{0:yyyy/MM/dd H:mm}", dateTime));
+        }
+
+        private static void DisplayPattern2(DateTime dateTime) {
+            Console.WriteLine(dateTime.ToString("yyyy年MM月dd日 H時mm分ss秒"));
+        }
+
+        private static void DisplayPattern3(DateTime dateTime) {
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+
+            Console.WriteLine(
+                dateTime.ToString($"gg y年 M月 d日（{culture.DateTimeFormat.GetDayName(dateTime.DayOfWeek)}）"
+                , culture)
+            );
+        }
+    }
+}
