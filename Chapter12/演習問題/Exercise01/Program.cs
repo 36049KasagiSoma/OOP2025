17e806ff215a35fa545fd9731c95f384c3521b5a
KasagiSoma <oibc36049@ojs.ac.jp>
2025-07-11 14:56:22 +0900

    12、リストじゃなくて配列だったので修正

--- Chapter12/演習問題/Exercise01/Program.cs
+++ Chapter12/演習問題/Exercise01/Program.cs
@@ -8,41 +8,38 @@ namespace Exercise01 {
         static void Main(string[] args) {
             Employee employee = new Employee {
                 Id = 0,
-                Name = "Yanagida Sota",
+                Name = "Sota Yanagida",
                 HireDate = new DateTime(1051, 2, 3)
             };
-            
+
             // 1
-            saveSerializeObject<Employee>("q1.json",employee);
+            saveSerializeObject<Employee>("q1.json", employee);
 
 
-            List<Employee> employees = new List<Employee>{
-                new Employee
-                {
+            Employee[] employees = new Employee[]{
+                new Employee{
                     Id = 1,
                     Name = "Alice Tanaka",
                     HireDate = new DateTime(2020, 5, 1)
                 },
-                new Employee
-                {
+                new Employee{
                     Id = 2,
                     Name = "Bob Yamada",
                     HireDate = new DateTime(2019, 3, 15)
                 },
-                new Employee
-                {
+                new Employee{
                     Id = 3,
                     Name = "Charlie Suzuki",
                     HireDate = new DateTime(2022, 11, 30)
                 }
             };
 
-            //2
-            saveSerializeObject<List<Employee>>("q2.json", employees);
+            // 2
+            saveSerializeObject<Employee[]>("q2.json", employees);
 
-            //3
-            List<Employee>? loadObj = loadDeserializeObject<List<Employee>>("q2.json");
-            loadObj?.ForEach(Console.WriteLine);
+            // 3
+            Employee[]? loadObj = loadDeserializeObject<Employee[]>("q2.json");
+            loadObj?.ToList().ForEach(Console.WriteLine);
         }
 
 
