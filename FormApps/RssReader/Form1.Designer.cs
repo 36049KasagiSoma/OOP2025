a80491953ce73aa11be176af0fdea02f484f955f
KasagiSoma <oibc36049@ojs.ac.jp>
2025-07-18 13:34:00 +0900

    Webページ表示完成

--- FormApps/RssReader/Form1.Designer.cs
+++ FormApps/RssReader/Form1.Designer.cs
@@ -27,24 +27,31 @@
             btRssGet = new Button();
             label1 = new Label();
             lbTitles = new ListBox();
+            webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            splitContainer1 = new SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)webView21).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
             SuspendLayout();
             // 
             // tbUrl
             // 
             tbUrl.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             tbUrl.Font = new Font("Yu Gothic UI", 12F);
-            tbUrl.Location = new Point(60, 12);
+            tbUrl.Location = new Point(60, 6);
             tbUrl.Name = "tbUrl";
-            tbUrl.Size = new Size(353, 29);
+            tbUrl.Size = new Size(325, 29);
             tbUrl.TabIndex = 0;
             // 
             // btRssGet
             // 
             btRssGet.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             btRssGet.Font = new Font("Yu Gothic UI", 12F);
-            btRssGet.Location = new Point(419, 11);
+            btRssGet.Location = new Point(391, 6);
             btRssGet.Name = "btRssGet";
-            btRssGet.Size = new Size(75, 30);
+            btRssGet.Size = new Size(103, 30);
             btRssGet.TabIndex = 1;
             btRssGet.Text = "取得";
             btRssGet.UseVisualStyleBackColor = true;
@@ -54,7 +61,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Yu Gothic UI", 12F);
-            label1.Location = new Point(12, 15);
+            label1.Location = new Point(12, 9);
             label1.Name = "label1";
             label1.Size = new Size(42, 21);
             label1.TabIndex = 2;
@@ -62,27 +69,63 @@
             // 
             // lbTitles
             // 
-            lbTitles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lbTitles.Dock = DockStyle.Fill;
             lbTitles.Font = new Font("Yu Gothic UI", 12F);
             lbTitles.FormattingEnabled = true;
             lbTitles.ItemHeight = 21;
-            lbTitles.Location = new Point(12, 47);
+            lbTitles.Location = new Point(0, 0);
             lbTitles.Name = "lbTitles";
-            lbTitles.Size = new Size(482, 382);
+            lbTitles.RightToLeft = RightToLeft.No;
+            lbTitles.Size = new Size(191, 396);
             lbTitles.TabIndex = 3;
+            lbTitles.HorizontalScrollbar = true;
             lbTitles.DoubleClick += lbTitles_DoubleClick;
+            // 
+            // webView21
+            // 
+            webView21.AllowExternalDrop = true;
+            webView21.CreationProperties = null;
+            webView21.DefaultBackgroundColor = Color.White;
+            webView21.Dock = DockStyle.Fill;
+            webView21.Location = new Point(0, 0);
+            webView21.Name = "webView21";
+            webView21.Size = new Size(287, 396);
+            webView21.TabIndex = 4;
+            webView21.ZoomFactor = 1D;
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            splitContainer1.Location = new Point(12, 42);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(lbTitles);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(webView21);
+            splitContainer1.Size = new Size(482, 396);
+            splitContainer1.SplitterDistance = 191;
+            splitContainer1.TabIndex = 5;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(506, 450);
-            Controls.Add(lbTitles);
-            Controls.Add(label1);
+            Controls.Add(splitContainer1);
             Controls.Add(btRssGet);
             Controls.Add(tbUrl);
+            Controls.Add(label1);
             Name = "Form1";
             Text = "RSSリーダー";
+            ((System.ComponentModel.ISupportInitialize)webView21).EndInit();
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -93,5 +136,7 @@
         private Button btRssGet;
         private Label label1;
         private ListBox lbTitles;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private SplitContainer splitContainer1;
     }
 }
--- FormApps/RssReader/Form1.cs
+++ FormApps/RssReader/Form1.cs
@@ -25,12 +25,11 @@ namespace RssReader {
                     var res = await hc.GetAsync(tbUrl.Text);
 
                     // 取得できなければ中断
-
                     res.EnsureSuccessStatusCode();
 
-                    var urlStr = await res.Content.ReadAsStringAsync();
+                    var xmlStr = await res.Content.ReadAsStringAsync();
 
-                    XDocument xdoc = XDocument.Parse(urlStr);
+                    XDocument xdoc = XDocument.Parse(xmlStr);
 
 #pragma warning disable CS8602 // null 参照の可能性があるものの逆参照です。
                     items = xdoc.Root.Descendants("item")
@@ -52,26 +51,33 @@ namespace RssReader {
                 }
             } catch (XmlException) {
                 MessageBox.Show("XMLを変換できませんでした。", "エラー",
-                MessageBoxButtons.OK,
-                MessageBoxIcon.Error);
-            } catch (HttpRequestException) {
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            } catch (Exception ex) when (ex is HttpRequestException || ex is InvalidOperationException) {
                 MessageBox.Show("データの取得に失敗しました。", "エラー",
-                MessageBoxButtons.OK,
-                MessageBoxIcon.Error);
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            } catch (Exception) {
+                MessageBox.Show("不明なエラーが発生しました。", "エラー",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
             } finally {
                 btRssGet.Enabled = true;
             }
+
         }
 
         private void lbTitles_DoubleClick(object sender, EventArgs e) {
             if (lbTitles.SelectedItems is null) return;
             var obj = lbTitles.SelectedItem;
             if (obj is ItemData data) {
-                Process.Start(
-                    new ProcessStartInfo(data.Link) {
-                        UseShellExecute = true
-                    }
-                );
+                //Process.Start(
+                //    new ProcessStartInfo(data.Link) {
+                //        UseShellExecute = true
+                //    }
+                //);
+                webView21.Source = new Uri(data.Link);
+
             }
         }
     }
