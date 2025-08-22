6bf56492d8bde8b5313f8f33ae79a1da164b426b
KasagiSoma <oibc36049@ojs.ac.jp>
2025-08-22 15:34:20 +0900

    11.3完成

---
+++ Chapter11/演習問題/Exercise03/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            string[] texts = [
+                "Time is money.",
+                "What time is it?",
+                "It will take time.",
+                "We reorganized the timetable.",
+            ];
+
+            foreach (string text in texts) {
+                int index = GetWordIndex(text, "time");
+                if (index >= 0) {
+                    Console.WriteLine($"{text} : {index}");
+                }
+            }
+
+        }
+
+        static int GetWordIndex(string text, string word) {
+            Regex regex = new Regex(@"\b" + word + @"\b", RegexOptions.IgnoreCase);
+            Match match = regex.Match(text);
+            if (match.Success) {
+                return match.Index;
+            }
+            return -1;
+        }
+    }
+}
