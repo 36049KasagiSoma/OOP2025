3b4647bd15998d9229063c6fc1c531c8d039ca49
KasagiSoma <oibc36049@ojs.ac.jp>
2025-08-22 14:08:44 +0900

    11.2完成

---
+++ Chapter11/演習問題/Exercise02/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Exercise02 {
+    internal class Program {
+        static void Main(string[] args) {
+            string filePath = "sample.txt";
+            Pickup3DigitNumber(filePath);
+        }
+
+        private static void Pickup3DigitNumber(string filePath) {
+            string text = File.ReadAllText(filePath);
+            var matches = Regex.Matches(text, @"\d{3,}");
+            foreach (Match m in matches) {
+                Console.WriteLine($"Index = {m.Index,-6}, Length = {m.Length,-3}, Value = {m.Value}");
+            }
+
+        }
+    }
+}
