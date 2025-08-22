a67ece5a7c46d72309874962b87f2b0f3c8079aa
KasagiSoma <oibc36049@ojs.ac.jp>
2025-08-22 10:28:57 +0900

    11.1入力

--- Chapter11/Section02/Program.cs
+++ Chapter11/Section02/Program.cs
@@ -1,7 +1,22 @@
-﻿namespace Section02 {
+﻿using System.Text.RegularExpressions;
+
+namespace Section02 {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            var text = "private List<string> results = new List<string>();";
+            var regEx = @"List<\w+>";
+            bool isMatch = Regex.IsMatch(text,regEx);
+            Console.WriteLine($"検索対象:{text}");
+            Console.WriteLine($"検索内容:{regEx}");
+            Console.Write("検索結果:");
+            if (isMatch) {
+                Console.WriteLine("見つかりました");
+            } else {
+                Console.WriteLine("見つかりません");
+            }
+
+
+
         }
     }
 }
