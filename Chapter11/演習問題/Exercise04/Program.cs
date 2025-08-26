419df509ab68e54267de5693ec9017718c59cbfd
KasagiSoma <oibc36049@ojs.ac.jp>
2025-08-26 15:01:12 +0900

    11.4\追加

--- Chapter11/演習問題/Exercise04/Program.cs
+++ Chapter11/演習問題/Exercise04/Program.cs
@@ -5,10 +5,10 @@ namespace Exercise04 {
         static void Main(string[] args) {
             string text = File.ReadAllText("sample.txt");
             string newText = Regex.Replace(text,
-                @"[vV]ersion\s*=\s*""v4.0""",
+                @"[vV]ersion\s*=\s*""v4\.0""",
                 @"version=""v5.0""");
             Console.WriteLine(newText);
-            File.WriteAllText("sample.txt", newText);
+            File.WriteAllText("sample2.txt", newText);
         }
     }
 }
