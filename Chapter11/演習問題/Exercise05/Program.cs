c9de487828f341828e973c10037675d9cc466777
KasagiSoma <oibc36049@ojs.ac.jp>
2025-08-26 15:45:47 +0900

    11.5 とりあえず完成

---
+++ Chapter11/演習問題/Exercise05/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Exercise05 {
+    internal class Program {
+        static void Main(string[] args) {
+            string text = File.ReadAllText("sample.html");
+
+            var tags = Regex.Matches(text, @"(</?[^>\s]*)(>|\s[^>]*)");
+
+            string work = text;
+            foreach (Match m in tags) {
+                work = work.Substring(0, m.Index)
+                    + m.Groups[1].Value.ToLower() + m.Groups[2].Value
+                    + work.Substring(m.Index + m.Length, work.Length - (m.Index + m.Length));
+            }
+
+            File.WriteAllText("sampleOut.html", work);
+
+        }
+    }
+}
