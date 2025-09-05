7ba242ac3f8e6477073f3ccab61f67e4ce5bbfc8
KasagiSoma <oibc36049@ojs.ac.jp>
2025-09-05 10:28:24 +0900

    イベント設定完了

--- WPF/SampleApplication/MainWindow.xaml.cs
+++ WPF/SampleApplication/MainWindow.xaml.cs
@@ -20,7 +20,39 @@ namespace SampleApplication {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+            setupDefaultSelection();
         }
 
+        // 初期値の設定
+        private void setupDefaultSelection() {
+            checkBox.IsChecked = false;
+            redRadioButton.IsChecked = true;
+            seasonConboBox.SelectedIndex = 0;
+            checkBox_Unchecked(checkBox, null);
+            radioButton_Checked(redRadioButton, null);
+            seasonTextBlock.Text = ((ComboBoxItem)seasonConboBox.SelectedItem).Content.ToString();
+        }
+
+        private void seasonConboBox_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+            seasonTextBlock.Text = ((ComboBoxItem)seasonConboBox.SelectedItem).Content.ToString();
+        }
+
+        private void radioButton_Checked(object sender, RoutedEventArgs e) {
+            if (!(sender is RadioButton)) {
+                Console.Error.WriteLine("sender is not RadioButton.");
+                Console.Error.WriteLine($"sender:{sender ?? "null"}");
+                Console.Error.WriteLine($"event:{e.ToString()}");
+                return;
+            }
+            colorText.Text = ((RadioButton)sender).Content.ToString();
+        }
+
+        private void checkBox_Checked(object sender, RoutedEventArgs e) {
+            chackbocTextBlock.Text = "チェックON";
+        }
+
+        private void checkBox_Unchecked(object sender, RoutedEventArgs e) {
+            chackbocTextBlock.Text = "チェックOFF";
+        }
     }
 }
