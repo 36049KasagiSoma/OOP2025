d8616a56d95e6b259b013be25d99f1fbfa365767
KasagiSoma <oibc36049@ojs.ac.jp>
2025-09-09 14:28:45 +0900

    ColorChecker 色のJson保存、右クリック処理など

---
+++ WPF/ColorChecker/JsonEvent.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace ColorChecker {
+    public class JsonEvent {
+        private JsonEvent() { }
+
+        /// <summary>
+        /// Jsonで保存します。
+        /// </summary>
+        public static T LoadItem<T>(string filePath) {
+            if (System.IO.File.Exists(filePath)) {
+                string jsonText = System.IO.File.ReadAllText(filePath);
+                return JsonSerializer.Deserialize<T>(jsonText,
+                    new JsonSerializerOptions {
+                        Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                    }
+                );
+            }
+            return default(T);
+        }
+
+        /// <summary>
+        /// Jsonを読み込みます。
+        /// </summary>
+        public static void SaveItem(string filePath, object item) {
+            string jsonText = JsonSerializer.Serialize(item,
+                new JsonSerializerOptions {
+                    Encoder = JavaScriptEncoder.Create(UnicodeRanges.All),
+                    WriteIndented = true
+                });
+            System.IO.File.WriteAllText(filePath, jsonText);
+        }
+    }
+}
--- WPF/ColorChecker/MainWindow.xaml.cs
+++ WPF/ColorChecker/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -30,7 +32,7 @@ namespace ColorChecker {
         private bool isUpdating = false;    // コンボボックス更新中フラグ
         private Border selectedBorder;      // 選択中のBorder
 
-        private void setupPreview() {
+        private void setupPreview() { // Preview用のBorderを生成
             stockBorder = new List<Border>();
             for (int i = 0; i < stockGrid.RowDefinitions.Count; i++) {
                 for (int j = 0; j < stockGrid.ColumnDefinitions.Count; j++) {
@@ -39,29 +41,82 @@ namespace ColorChecker {
                     border.BorderBrush = Brushes.Gray;
                     border.BorderThickness = new Thickness(1);
                     border.ToolTip = $"R:255,G:255,B:255";  // 初期ToolTip
-                    //border.Background = null;
+
                     border.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
                     Grid.SetRow(border, i);
                     Grid.SetColumn(border, j);
                     border.MouseLeftButtonDown += (s, e) => {
                         Color color = ((SolidColorBrush)this.Background).Color;
                         if (e.ClickCount == 2) {
-                            colorComboBox.SelectedIndex = -1;
-                            colorPreview.Background = border.Background;
-                            SolidColorBrush brush = (SolidColorBrush)border.Background;
-                            redSlider.Value = brush.Color.R;
-                            greenSlider.Value = brush.Color.G;
-                            blueSlider.Value = brush.Color.B;
-                            updateTextBox();
+                            updatePreviewFromBorder(border);
                         } else if (e.ClickCount == 1) {
                             updateSelectBorder(border);
                         }
                     };
 
+                    ContextMenu contextMenu = new ContextMenu(); // 右クリックメニュー
+                    MenuItem item_select = new MenuItem { Header = "選択" };
+                    MenuItem item_reflection = new MenuItem { Header = "反映" };
+                    MenuItem item_copy = new MenuItem { Header = "コピー" };
+                    MenuItem item_copy_10code = new MenuItem { Header = "10進コード" };
+                    MenuItem item_copy_16code = new MenuItem { Header = "16進コード" };
+                    MenuItem item_clear = new MenuItem { Header = "クリア" };
+                    item_select.Click += (s, e) => updateSelectBorder(border);
+                    item_reflection.Click += (s, e) => {
+                        updateSelectBorder(border);         // 選択して
+                        updatePreviewFromBorder(border);    // 色を反映
+                    };
+                    item_copy_10code.Click += (s, e) => {
+                        Color color = ((SolidColorBrush)border.Background).Color;
+                        Clipboard.SetText($"{color.R}, {color.G}, {color.B}");
+                    };
+                    item_copy_16code.Click += (s, e) => {
+                        Color color = ((SolidColorBrush)border.Background).Color;
+                        Clipboard.SetText($"#{color.R:X2}{color.G:X2}{color.B:X2}");
+                    };
+                    item_clear.Click += (s, e) => {
+                        border.Background = new SolidColorBrush(Color.FromRgb(255, 255, 255));
+                        border.ToolTip = $"R:255,G:255,B:255";
+                        if (selectedBorder == border) selectedBorder = null;
+                        border.BorderBrush = Brushes.Gray;
+                        border.BorderThickness = new Thickness(1);
+                    };
+
+                    contextMenu.Items.Add(item_select);
+                    contextMenu.Items.Add(item_reflection);
+                    contextMenu.Items.Add(item_copy);
+                    item_copy.Items.Add(item_copy_10code);
+                    item_copy.Items.Add(item_copy_16code);
+                    contextMenu.Items.Add(new Separator());
+                    contextMenu.Items.Add(item_clear);
+                    border.ContextMenu = contextMenu;
+
+
                     stockGrid.Children.Add(border);
                     stockBorder.Add(border);
                 }
             }
+
+            //Jsonから保存データを読み込み
+            if (File.Exists("stocks.json")) {
+                Color[] brush = JsonEvent.LoadItem<Color[]>("stocks.json");
+                if (brush != null) {
+                    for (int i = 0; i < brush.Length && i < stockBorder.Count; i++) {
+                        stockBorder[i].Background = new SolidColorBrush(brush[i]);
+                        stockBorder[i].ToolTip = $"R:{brush[i].R},G:{brush[i].G},B:{brush[i].B}";
+                    }
+                }
+            }
+        }
+
+        private void updatePreviewFromBorder(Border border) {
+            colorComboBox.SelectedIndex = -1;
+            colorPreview.Background = border.Background;
+            SolidColorBrush brush = (SolidColorBrush)border.Background;
+            redSlider.Value = brush.Color.R;
+            greenSlider.Value = brush.Color.G;
+            blueSlider.Value = brush.Color.B;
+            updateTextBox();
         }
 
         private void updateSelectBorder(Border highlight) {
@@ -88,19 +143,15 @@ namespace ColorChecker {
 
         private void Button_Click(object sender, RoutedEventArgs e) {
             //shiftPanel();
-            if (selectedBorder == null) return;
+            if (selectedBorder == null) {
+                MessageBox.Show("色を設定する枠を選択してください。", "情報", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
             selectedBorder.Background = colorPreview.Background;
             SolidColorBrush brush = (SolidColorBrush)selectedBorder.Background;
             selectedBorder.ToolTip = $"R:{brush.Color.R},G:{brush.Color.G},B:{brush.Color.B}";
 
         }
-        //private void shiftPanel() {
-        //    for (int i = stockBorder.Count - 1; i > 0; i--) {
-        //        Border c = stockBorder[i];
-        //        Border p = stockBorder[i - 1];
-        //        c.Background = p.Background;
-        //    }
-        //}
 
         private void updateTextBox() {
             SolidColorBrush brush = (SolidColorBrush)colorPreview.Background;
@@ -125,10 +176,10 @@ namespace ColorChecker {
 
         public static Dictionary<string, Brush> MakeBrushesDictionary() {
             //Brushesの情報(PublicとStaticなプロパティ全部)取得
-            System.Reflection.PropertyInfo[] brushInfos =
+            PropertyInfo[] brushInfos =
                 typeof(Brushes).GetProperties(
-                    System.Reflection.BindingFlags.Public |
-                    System.Reflection.BindingFlags.Static);
+                    BindingFlags.Public |
+                    BindingFlags.Static);
 
             Dictionary<string, Brush> dict = new Dictionary<string, Brush>();
             foreach (var item in brushInfos) {
@@ -139,8 +190,52 @@ namespace ColorChecker {
             return dict;
         }
 
-        private void color10code_KeyDown(object sender, KeyEventArgs e) {
+        private void color10code_PreviewKeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                string[] rgb = color10code.Text.Split(',');
+                if (rgb.Length != 3) return;
+                if (byte.TryParse(rgb[0], out byte r) &&
+                    byte.TryParse(rgb[1], out byte g) &&
+                    byte.TryParse(rgb[2], out byte b)) {
+                    isUpdating = true;
+                    colorPreview.Background = new SolidColorBrush(Color.FromRgb(r, g, b));
+                    redSlider.Value = r;
+                    greenSlider.Value = g;
+                    blueSlider.Value = b;
+                    colorComboBox.SelectedIndex = -1;
+                    isUpdating = false;
+                }
+            }
+        }
 
+        private void color16code_PreviewKeyDown(object sender, KeyEventArgs e) {
+            if (e.Key == Key.Enter) {
+                string hex = color16code.Text.Trim();
+                if (!hex.StartsWith("#")) {
+                    color16code.Text = "#" + hex;
+                    hex = "#" + hex;
+                }
+                if (!Regex.IsMatch(hex, @"^#[0-9a-fA-F]{6}$")) return;
+                isUpdating = true;
+                object obj = ColorConverter.ConvertFromString(hex);
+                var color = (Color)obj;
+                colorPreview.Background = new SolidColorBrush(color);
+                redSlider.Value = color.R;
+                greenSlider.Value = color.G;
+                blueSlider.Value = color.B;
+                colorComboBox.SelectedIndex = -1;
+                isUpdating = false;
+            }
+        }
+
+        private void Button_10Code_Copy_Click(object sender, RoutedEventArgs e)
+            => Clipboard.SetText(color10code.Text);
+        private void Button_16Code_Copy_Click(object sender, RoutedEventArgs e)
+            => Clipboard.SetText(color16code.Text);
+
+        private void Window_Closed(object sender, EventArgs e) {
+            Color[] brush = stockBorder.Select(b => ((SolidColorBrush)b.Background).Color).ToArray();
+            JsonEvent.SaveItem("stocks.json", brush);
         }
     }
 }
