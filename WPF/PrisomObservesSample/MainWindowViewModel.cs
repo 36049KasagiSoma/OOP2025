068ff88964d8eab7c120d143aff91006cf0e1f82
KasagiSoma <oibc36049@ojs.ac.jp>
2025-10-06 15:40:10 +0900

    たぶんCan

--- WPF/PrisomObservesSample/MainWindowViewModel.cs
+++ WPF/PrisomObservesSample/MainWindowViewModel.cs
@@ -10,37 +10,51 @@ namespace PrisomObservesSample {
         private string _input2 = "";
         private string _result = "";
 
-        public string Input1 { get => _input1; set => SetProperty(ref _input1, value); }
-        public string Input2 { get => _input2; set => SetProperty(ref _input2, value); }
+        public string Input1 {
+            get => _input1; set {
+                SetProperty(ref _input1, value);
+                SumCommand.RaiseCanExecuteChanged();
+            }
+        }
+        public string Input2 {
+            get => _input2; set {
+                SetProperty(ref _input2, value);
+                SumCommand.RaiseCanExecuteChanged();
+            }
+        }
         public string Result { get => _result; set => SetProperty(ref _result, value); }
 
         public MainWindowViewModel() {
-            SumCommand = new DelegateCommand(ExcuteSum);
+            SumCommand = new DelegateCommand(ExcuteSum, CanExcuteSum);
         }
+
+        private bool CanExcuteSum() {
+            return int.TryParse(Input1, out _) && int.TryParse(Input2, out _);
+        }
+
 
         private void ExcuteSum() {
             if (int.TryParse(Input1, out int in1) && int.TryParse(Input2, out int in2)) {
                 Result = (in1 + in2).ToString();
             } else {
                 Result = "?";
-                //new Thread(() => { 
-                //    for (int i = 0; i < 1000; i++) {
-                //        string jet = "";
-                //        for (int j = 0; j < i % 10; j++) {
-                //            jet += "～";
-                //        }
-                //        if (i % 2 == 0) {
-                //            Result = jet + "＼(^o^)＼";
-                //        } else {
-                //            Result = jet + "／(^o^)／";
-                //        }
-                //        Thread.Sleep(200);
-                //    }
-                //}).Start();
+                new Thread(() => {
+                    for (int i = 0; i < 1000; i++) {
+                        string jet = "";
+                        for (int j = 0; j < i % 10; j++) {
+                            jet += "～";
+                        }
+                        if (i % 2 == 0) {
+                            Result = jet + "＼(^o^)＼";
+                        } else {
+                            Result = jet + "／(^o^)／";
+                        }
+                        Thread.Sleep(200);
+                    }
+                }).Start();
             }
         }
 
         public DelegateCommand SumCommand { get; }
-
     }
 }
