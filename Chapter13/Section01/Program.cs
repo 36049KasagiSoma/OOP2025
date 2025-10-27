8e0c19486a54c47b4cba49808e66f400e0d76e8c
KasagiSoma <oibc36049@ojs.ac.jp>
2025-10-27 15:29:23 +0900

    13.1.1、13.1.2、13.1.3終わり

---
+++ Chapter13/Section01/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var price = Library.Books.Where(b => b.CategoryId == 1).Max(b => b.Price);
+            Console.WriteLine(price);
+            Console.WriteLine();
+
+            var book = Library.Books.Where(b => b.PublishedYear >= 2021).MinBy(b => b.Price);
+            Console.WriteLine(book);
+            Console.WriteLine();
+
+            var avg = Library.Books.Average(b => b.Price);
+
+            var books = Library.Books.Where(b => b.Price > avg);
+            books.ToList().ForEach(Console.WriteLine);
+
+        }
+    }
+}
---
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            Exercise1_2();
+            Console.WriteLine();
+            Exercise1_3();
+            Console.WriteLine();
+            Exercise1_4();
+            Console.WriteLine();
+            Exercise1_5();
+            Console.WriteLine();
+            Exercise1_6();
+            Console.WriteLine();
+            Exercise1_7();
+            Console.WriteLine();
+            Exercise1_8();
+
+            Console.ReadLine();
+
+
+        }
+
+        private static void Exercise1_2() {
+            Console.WriteLine(Library.Books.MaxBy(b => b.Price));
+        }
+
+        private static void Exercise1_3() {
+            Library.Books.GroupBy(b => b.PublishedYear)
+                .Select(x => $"{x.Key}:{x.Count()}")
+                .ToList().ForEach(Console.WriteLine);
+        }
+
+        private static void Exercise1_4() {
+
+        }
+
+        private static void Exercise1_5() {
+
+        }
+
+        private static void Exercise1_6() {
+
+        }
+
+        private static void Exercise1_7() {
+
+        }
+
+        private static void Exercise1_8() {
+
+        }
+    }
+}
