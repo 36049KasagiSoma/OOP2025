db29f8f0b711bc30438b4d346d5ec6018e26dcc0
KasagiSoma <oibc36049@ojs.ac.jp>
2025-10-29 09:31:13 +0900

    13.1.8まで

--- Chapter13/演習問題/Exercise01/Program.cs
+++ Chapter13/演習問題/Exercise01/Program.cs
@@ -45,15 +45,28 @@ namespace Exercise01 {
         }
 
         private static void Exercise1_6() {
-            
+            Library.Books.GroupBy(b => b.CategoryId).ToList().ForEach(g => {
+                Console.WriteLine($"# {Library.Categories.Where(c => c.Id == g.Key).First().Name}");
+                g.ToList().ForEach(b => Console.WriteLine($"  {b.Title}"));
+            });
         }
 
         private static void Exercise1_7() {
-
+            Library.Books.Where(b => b.CategoryId
+                    == Library.Categories.Where(c => c.Name == "Development").First().Id)
+                .GroupBy(b => b.PublishedYear).ToList().ForEach(g => {
+                    Console.WriteLine($"# {g.Key}");
+                    g.ToList().ForEach(b => Console.WriteLine($"  {b.Title}"));
+                }
+            );
         }
 
         private static void Exercise1_8() {
-
+            Library.Categories.GroupJoin(
+                Library.Books, c => c.Id,
+                b => b.CategoryId, (c, b) => new { Category = c.Name, Books = b }
+            ).Where(g => g.Books.Count() >= 4)
+            .ToList().ForEach(g => Console.WriteLine(g.Category));
         }
     }
 }
