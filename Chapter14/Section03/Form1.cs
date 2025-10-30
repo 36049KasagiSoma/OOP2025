43d962f68094b80b59ea425a462e93fef82df42b
KasagiSoma <oibc36049@ojs.ac.jp>
2025-10-30 15:17:45 +0900

    14.9

--- Chapter14/Section03/Form1.cs
+++ Chapter14/Section03/Form1.cs
@@ -9,12 +9,15 @@ namespace Section03 {
 
         private async void button1_Click(object sender, EventArgs e) {
             toolStripStatusLabel1.Text = "";
-            await DoLongTimeWork();
-            toolStripStatusLabel1.Text = $"I—¹";
+            var elapsed = await DoLongTimeWorkAsync(4000);
+            toolStripStatusLabel1.Text = $"{elapsed}ƒ~ƒŠ•b";
         }
 
-        private async Task DoLongTimeWork() {
-            await Task.Run(() => Thread.Sleep(5000));
+        private async Task<long> DoLongTimeWorkAsync(int v) {
+            var sw = Stopwatch.StartNew();
+            await Task.Run(() => Thread.Sleep(v));
+            sw.Stop();
+            return sw.ElapsedMilliseconds;
         }
     }
 }
