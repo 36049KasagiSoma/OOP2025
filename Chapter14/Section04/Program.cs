48c55aaa1452234c930361a5fead7915b52519bc
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-05 11:16:57 +0900

    Code14-13

---
+++ Chapter14/Section04/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Section04 {
+    internal class Program {
+        static async Task Main(string[] args) {
+            await GetHtmlExample(new HttpClient());
+        }
+
+        static async Task GetHtmlExample(HttpClient client) {
+            var url = "https://abehiroshi.la.coocan.jp/";
+            var text= await client.GetStringAsync(url);
+            Console.WriteLine(text);
+        }
+    }
+}
