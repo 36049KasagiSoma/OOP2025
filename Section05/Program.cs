2a07d15e0cef9160dcab4b88af8953ad5046a876
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-06 15:59:17 +0900

    HttpC並列

--- Section05/Program.cs
+++ Section05/Program.cs
@@ -4,12 +4,36 @@ using System.Threading.Tasks;
 namespace Section05 {
     internal class Program {
         static async Task Main(string[] args) {
-            await Code14_20.Run();
+            await new TaskExample().Run();
 
 
         }
     }
 
+
+    // Code 14.22
+    public class TaskExample {
+        private readonly HttpClient _httpClient = new HttpClient();
+
+        public async Task Run() {
+            var tasks = new Task<string>[] {
+                GetPageAsync(@"https://business.nikkei.com/rss/sns/nb.rdf"),
+                GetPageAsync(@"https://www.nhk.or.jp/rss/news/cat3.xml"),
+            };
+            var results = await Task.WhenAll(tasks);
+
+            // それぞれ先頭400文字を表示する
+            var text = $"{results[0].Substring(0, 400)}\n\n{results[1].Substring(0, 400)}";
+            Console.WriteLine(text);
+        }
+
+
+        private async Task<string> GetPageAsync(string urlstr) {
+            var str = await _httpClient.GetStringAsync(urlstr);
+            return str;
+        }
+
+    }
 
     public class Code14_20 {
         public static async Task Run() {
