45e6463005065a82571dca4af91fa1a7a76ce969
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-10 13:12:38 +0900

    10.1完成

---
+++ Chapter10/演習問題/Exercise01/CountClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    public class CountClass {
+        private string _path;
+        public CountClass(string path) {
+            this._path = path;
+        }
+
+        public int CountClassStr() {
+            int cnt = 0;
+            
+            using(StreamReader reader = new StreamReader(_path)) {
+                string? line;
+                while((line = reader.ReadLine()) != null) {
+                    if(Regex.IsMatch(line, @"\sclass\s")) {
+                        cnt++;
+                    }
+                }
+            }
+
+            return cnt;
+        }
+    }
+}
---
+++ Chapter10/演習問題/Exercise01/Program.cs
@@ -0,0 +1,8 @@
+﻿namespace Exercise01 {
+    internal class Program {
+        static void Main(string[] args) {
+            int cnt = new CountClass("source.txt").CountClassStr();
+            Console.WriteLine($"classの数:{cnt}");
+        }
+    }
+}
