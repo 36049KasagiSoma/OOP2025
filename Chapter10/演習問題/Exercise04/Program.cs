fe17b7fa3e1d8e2a36a29bc7ec337c0aa28f5302
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-10 13:48:43 +0900

    10.4完成

---
+++ Chapter10/演習問題/Exercise04/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Exercise04 {
+    internal class Program {
+        static void Main(string[] args) {
+            string path = string.Empty;
+            const long megabyte = 1048576;
+            do {
+                Console.Write("path1:");
+                path = Console.ReadLine() ?? string.Empty;
+            } while (!Directory.Exists(path));
+
+            List<string> files = GetAllFiles(path);
+            foreach (string file in files) {
+                var size = new FileInfo(file).Length;
+                if (size >= megabyte) {
+                    string name = Path.GetFileName(file);
+                    Console.WriteLine($"{size/megabyte,5}MB({size,10}B):{name}");
+                }
+
+            }
+        }
+
+        static List<string> GetAllFiles(string path) {
+            List<string> files = new List<string>();
+            foreach (var file in Directory.GetFiles(path)) {
+                if(File.Exists(file))
+                    files.Add(file);
+            }
+            foreach (var dir in Directory.GetDirectories(path)) {
+                if(Directory.Exists(dir))
+                    files.AddRange(GetAllFiles(dir));
+            }
+            return files;
+        }
+    }
+}
