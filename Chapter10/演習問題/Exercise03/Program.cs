5befb6c7952e1780cf976b41c9a8da9b6b1d9669
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-10 13:33:49 +0900

    10.3完成

---
+++ Chapter10/演習問題/Exercise03/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exercise03 {
+    internal class Program {
+        static void Main(string[] args) {
+            string path1 = string.Empty;
+            do {
+                Console.Write("path1:");
+                path1 = Console.ReadLine() ?? string.Empty;
+            } while (!File.Exists(path1));
+
+            string path2 = string.Empty;
+            do {
+                Console.Write("path2:");
+                path2 = Console.ReadLine() ?? string.Empty;
+            } while (!File.Exists(path2));
+
+            string outPath = string.Empty;
+            Console.Write("out:");
+            outPath = Console.ReadLine() ?? string.Empty;
+
+            var lines1 = File.ReadAllLines(path1);
+            var lines2 = File.ReadAllLines(path2);
+            using (StreamWriter writer = new StreamWriter(outPath)) {
+                foreach (var line in lines1) {
+                    writer.WriteLine(line);
+                }
+                foreach (var line in lines2) {
+                    writer.WriteLine(line);
+                }
+            }
+        }
+    }
+}
