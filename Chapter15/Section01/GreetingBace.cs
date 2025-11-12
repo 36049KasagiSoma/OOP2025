f03aeda401621a98601d31f63ac0867f5f3ff62b
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-12 10:12:37 +0900

    継承をつかったポリモーフィズム

---
+++ Chapter15/Section01/GreetingBace.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section01 {
+    public abstract class GreetingBace {
+        public virtual string GetMessage() => string.Empty;
+    }
+}
---
+++ Chapter15/Section01/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+            List<GreetingBace> list = [
+                new GreetingMorning(),
+                new GreetingAfternoon(),
+                new GreetingEvening(),
+            ];
+
+            foreach (var obj in list) {
+                string msg = obj.GetMessage();
+                Console.WriteLine(msg);
+            }
+
+        }
+
+        class GreetingMorning:GreetingBace {
+            public override string GetMessage() => "おはよう";
+        }
+        class GreetingAfternoon : GreetingBace {
+            public override string GetMessage() => "こんにちは";
+        }
+        class GreetingEvening : GreetingBace {
+            public override string GetMessage() => "こんばんは";
+        }
+    }
+}
