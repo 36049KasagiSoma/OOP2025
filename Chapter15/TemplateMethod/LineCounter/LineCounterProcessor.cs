3b7b4527db612102dc2e4f9c02576f2e0373bfb4
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-12 11:42:48 +0900

    ①②完成

--- Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using TextFileProcessor;
 
 namespace LineCounter {
     public class LineCounterProcessor : TextProcessor {
         private int _cnt = 0;
+        private string _word = string.Empty;
 
-        protected override void Initialize(string fname) => _cnt = 0;
-        protected override void Execute(string line) => _cnt++;
+        protected override void Initialize(string fname) {
+            _cnt = 0;
+            Console.Write("検索文字列 > ");
+            _word = Console.ReadLine()??string.Empty;
+        }
+        protected override void Execute(string line) {
+            _cnt += Regex.Matches(line, @$".*{_word}.*").Count();
+        }
         protected override void Terminate() => Console.WriteLine($"{_cnt} 行");
 
         public int GetCount() => _cnt;
