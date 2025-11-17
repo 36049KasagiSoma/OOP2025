87c372ab48cdd1e6a6e3444c54c226ba30d5f865
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-17 15:30:20 +0900

    15.3完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineOutputService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class LineOutputService : ITextFileService {
+        private int _count;
+        private int _maxCount;
+        public LineOutputService(int maxLineCount) {
+            _maxCount = maxLineCount;
+        }
+        public void Execute(string line) {
+            _count++;
+            if (_count <= _maxCount)
+                Console.WriteLine($"{GetlineNum(_count, _maxCount.ToString().Length)}|{line}");
+        }
+
+        public void Initialize(string fname) {
+            _count = 0;
+        }
+
+        public void Terminate() {
+        }
+
+        private string GetlineNum(int num, int length) {
+            StringBuilder space = new StringBuilder();
+            while (num.ToString().Length + space.Length < length) {
+                space.Append(' ');
+            }
+            return space + num.ToString();
+        }
+    }
+}
--- Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace TextFileProcessorDI {
     internal class Program {
         static void Main(string[] args) {
-            var service = new LineCounterService();
+            var service = new LineOutputService(20);
             TextFileProcessor processor = new TextFileProcessor(service);
+            
+            string? inputPath = null;
             Console.Write("パスを入力:");
-            processor.Run(Console.ReadLine() ?? "sample.txt");
+            
+            do {
+                inputPath = Console.ReadLine();
+            } while (inputPath is null || !File.Exists(inputPath));
+            
+            processor.Run(inputPath);
+
         }
     }
 }
