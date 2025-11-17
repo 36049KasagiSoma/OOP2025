5c8d6830b3be8b01de379aaa63bb0a0ce964806d
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-17 15:05:48 +0900

    依存性の注入パターン

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TextFileProcessorDI {
+    internal class Program {
+        static void Main(string[] args) {
+            var service = new LineCounterService();
+            TextFileProcessor processor = new TextFileProcessor(service);
+            Console.Write("パスを入力:");
+            processor.Run(Console.ReadLine() ?? "sample.txt");
+        }
+    }
+}
---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/TextFileProcessor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    public class TextFileProcessor {
+        private ITextFileService _textFileService;
+
+        public TextFileProcessor(ITextFileService service) {
+            _textFileService = service;
+        }
+
+        public void Run(string fileName){
+            _textFileService.Initialize(fileName);
+            var lines = File.ReadLines(fileName);
+            foreach (var line in lines) {
+                _textFileService.Execute(line);
+            }
+            _textFileService.Terminate();
+        } 
+    }
+}
