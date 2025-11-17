a70f5e8f03818e41ff10114e6594b4d97dbb82fe
KasagiSoma <oibc36049@ojs.ac.jp>
2025-11-17 13:34:35 +0900

    15.1 完成

---
+++ Chapter15/演習問題/Exercise01/MyTextProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using TextFileProcessor;
+
+namespace Exercise01 {
+    public class MyTextProcessor:TextProcessor {
+        protected override void Execute(string line) {
+            string newLine = Regex.Replace(line, @"[０-９]",
+                p => ((char)(p.Value[0] - '０' + '0')).ToString());
+            Console.WriteLine(newLine);
+        }
+    }
+}
---
+++ Chapter15/演習問題/Exercise01/Program.cs
@@ -0,0 +1,10 @@
+﻿using TextFileProcessor;
+
+namespace Exercise01 {
+
+    internal class Program {
+        static void Main(string[] args) {
+            TextProcessor.Run<MyTextProcessor>("test.txt");
+        }
+    }
+}
